89200e0b589975ca8b075dfef25a46809de7f402
Yanni TheDeveloper <44473705+YanniTheDeveloper@users.noreply.github.com>
2021-05-14 13:15:22 -0400

    # User repository implemented

--- lib/data/repositories/user_repository.dart
+++ lib/data/repositories/user_repository.dart
@@ -1,24 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:gorin_users/data/models/user.dart';
 
-abstract class UserRepositoryInterface{
-  Stream getUsers();
-  bool createUser(User user);
+abstract class UserRepositoryInterface {
+  Stream<List<User>> getUsers();
+  Future<void> createUser(User user);
 }
 
 class UserRepository implements UserRepositoryInterface {
   CollectionReference usersRef = FirebaseFirestore.instance.collection("users");
 
   @override
-  bool createUser(User user) {
-    // TODO: implement createUser
-    throw UnimplementedError();
-  }
+  Future<void> createUser(User user) async => usersRef.add(user.toJson());
 
   @override
-  Stream getUsers() {
-    // TODO: implement getUsers
-    throw UnimplementedError();
-  }
-
+  Stream<List<User>> getUsers() => usersRef.snapshots().map((usersSnapshot) =>
+      usersSnapshot.docs.map((user) => User.fromJson(user.data())).toList());
 }
