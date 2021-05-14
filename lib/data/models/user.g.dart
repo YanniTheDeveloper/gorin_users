2eb1a87aa851451dcbc9d1c1cf55b2378c131e82
Yanni TheDeveloper <44473705+YanniTheDeveloper@users.noreply.github.com>
2021-05-14 13:05:17 -0400

    user.g.dart generated

---
+++ lib/data/models/user.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) {
+  return User(
+    id: json['id'] as String,
+    name: json['name'] as String,
+    email: json['email'] as String,
+    imageUrl: json['imageUrl'] as String,
+    joined: const TimestampConverter().fromJson(json['joined']),
+  );
+}
+
+Map<String, dynamic> _$UserToJson(User instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('id', instance.id);
+  writeNotNull('name', instance.name);
+  writeNotNull('email', instance.email);
+  writeNotNull('imageUrl', instance.imageUrl);
+  writeNotNull('joined', const TimestampConverter().toJson(instance.joined));
+  return val;
+}
